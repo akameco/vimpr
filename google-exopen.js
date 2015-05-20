264c401d9a0346d0d0828d1282934a2410107af6
akameco <akameco.t@gmail.com>
2015-05-20 13:16:56 +0900

    Update

--- google-exopen.js
+++ google-exopen.js
@@ -11,28 +11,27 @@ let PLUGIN_INFO = xml`
 // }}}
 
 (function () {
+    let original = mappings.getDefault(modes.NORMAL, 'o');
+
     mappings.addUserMap(
       [modes.NORMAL],['o'],':open',
-      function() { 
+      function() {
         // urlを取得
-        var url = window.content.window.location;
-        // コマンドの引数
-        var commandPram = '';
+        let url = window.content.window.location;
         // google検索か判定
-        if(url.host === 'www.google.co.jp') {
-          // クエリ部の抜き出し
-          var q = decodeURI(url.href).match(/q=(.*?)&/);
-          // foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
-          var commandPram = q[1].replace(/\+/g,' ');
+        if(url.host !== 'www.google.co.jp') {
+          return original.action.apply(this, arguments);
         }
+
+        // クエリ部の抜き出し
+        let q = url.href.match(/[?&]q=(.*?)&/);
+        // foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
+        let commandPram = decodeURIComponent(q[1]).replace(/\+/g,' ');
+
         // コマンドの生成
-        var command = 'open ' + commandPram;
+        let command = 'open ' + commandPram;
         commandline.open('',
-          commands.commandToString(
-            {
-              command: command
-            }
-        ),modes.EX);
+          commands.commandToString({command: command}),modes.EX);
       }
     );
 })();
